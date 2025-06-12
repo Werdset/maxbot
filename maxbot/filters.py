6d89a34f44040590d07ae6cc4b4c03a9aabebf15
werdset <vladimirwerdset@gmail.com>
2025-06-12 16:36:44 +0300

    push

--- maxbot/filters.py
+++ maxbot/filters.py
@@ -1,26 +1,25 @@
 class FilterExpression:
-    def __init__(self, attr: str):
+    def __init__(self, attr: str, op: str = None, value: any = None):
         self.attr = attr
-        self.op = None
-        self.value = None
+        self.op = op
+        self.value = value
 
     def __eq__(self, other):
-        self.op = "eq"
-        self.value = other
-        return self
+        return FilterExpression(self.attr, "eq", other)
 
     def check(self, data):
         value = data
         for part in self.attr.split("."):
             value = getattr(value, part, None)
+
         if self.op == "eq":
             return value == self.value
+
         return False
 
 
 class FMeta:
     def __getattr__(self, item):
         return FilterExpression(item)
-
 
 F = FMeta()
